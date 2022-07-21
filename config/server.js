bcd947438b8a44402c873682136a9cdca0ea95fa
Adam Horne <adam@madamot.com>
2022-07-21 15:10:44 +0100

    before codemod

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '7d6deacc33ebf80e0e9069f670c33161'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
